83bbefbf11ba8c9f0e8f7af9cb36dfb27f016e17
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-08-25 16:21:29 +0300

    Add my-component

---
+++ my-component/README.md
@@ -0,0 +1,4 @@
+## Quasar App Extension MyComponent
+
+This is an example of a Quasar App Extension that injects a custom component into the hosting Quasar app.
+
---
+++ my-component/src/boot/boot-file.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import MyComponent from './src/component/MyComponent.vue'
+
+// we globally register our component
+Vue.component('MyComponent', MyComponent)
