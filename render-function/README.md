13691946ea881b5d6a4ea65d048d8741772a1e46
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-08-28 09:58:06 -0600

    feat(example): adding render function example

---
+++ render-function/README.md
@@ -0,0 +1,6 @@
+## Quasar App Extension RenderFunction
+
+This is an example of a Quasar App Extension that use the API Render function to copy directly to the hosting apps folders.
+
+Take into the account that for this example, the `ext-id` of this App Extension is `render-function` (this can be anything, and please do change it for a more appropriate name for your App Extension).
+
---
+++ render-function/src/install.js
@@ -0,0 +1,27 @@
+/**
+ * Quasar App Extension install script
+ *
+ * Docs: https://quasar.dev/app-extensions/development-guide/install-api
+ * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/InstallAPI.js
+ */
+
+const { existsSync } = require('fs')
+
+module.exports = function (api) {
+  api.compatibleWith('@quasar/app', '^1.0.0')
+
+  let pageExists = false
+
+  // we can use 'api.resolve.src' to see if "SpecialPage.vue already exists"
+  // https://quasar.dev/app-extensions/development-guide/install-api#api.resolve
+  if (existsSync(api.resolve.app('pages/SpecialPage.vue'))) {
+    pageExists = true
+  }
+
+  // on subsequent installs, the user will be asked to overwrite this file
+  api.render('./templates/pages')
+
+  // if the page didn't originally exist, show a message
+  // https://quasar.dev/app-extensions/development-guide/install-api#api.onExitLog
+  api.onExitLog('Be sure to add "SpecialPage.vue" to your routes.')
+}
