0ba1faecbdda8d6e5399f2dfd70733eee0f1f751
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-09-12 15:18:07 +0300

    feat: Additions to my-starter-kit

--- my-starter-kit/src/uninstall.js
+++ my-starter-kit/src/uninstall.js
@@ -5,6 +5,31 @@
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/UninstallAPI.js
  */
 
+// we yarn added it to our App Extension,
+// so we can import the following:
+const rimraf = require('rimraf')
+
 module.exports = function (api) {
-  //
+  // Careful when you remove folders!
+  // You don't want to delete files that are still needed by the Project,
+  // or files that are not owned by this app extension.
+
+
+  // Here, we could also remove the /src/services folder altogether,
+  // but what if the user has added other files into this folder?
+
+  if (api.prompts.serviceA) {
+    // we added it on install, so we remove it
+    rimraf.sync(api.resolve.src('services/serviceA.js'))
+  }
+
+  if (api.prompts.serviceB) {
+    // we added it on install, so we remove it
+    rimraf.sync(api.resolve.src('services/serviceB.js'))
+  }
+
+  // we added it on install, so we remove it
+  rimraf.sync(api.resolve.app('some-folder'))
+  // warning... we've added this folder, but what if the
+  // developer added more files into this folder???
 }
