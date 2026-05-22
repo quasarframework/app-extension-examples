d5d017867cc171b2dc4bd7b4ae3821a52bb12af6
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2026-05-22 12:44:18 +0300

    feat: update to latest TS specs

---
+++ my-starter-kit/typescript-variant/playground/env.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Add types (that are not auto-magically added by Quasar CLI already)
+ * for your custom variables to avoid TypeScript errors, like dynamic
+ * process.env variables or definitions in dotenv files configured ONLY
+ * for the /quasar.config file itself.
+ *
+ * @example
+ * interface ImportMetaEnv {
+ *   readonly MY_VAR: string;
+ *   readonly MY_OTHER_VAR: string;
+ * }
+ */
+interface ImportMetaEnv {}
