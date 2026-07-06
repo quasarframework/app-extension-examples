e80bb02e9fea427ed8031455df3102a972ab1244
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2026-07-06 11:23:25 +0300

    chore: update app-vite v3 version

--- my-starter-kit/javascript-variant/ae/src/index.js
+++ my-starter-kit/javascript-variant/ae/src/index.js
@@ -43,7 +43,7 @@ export default defineIndexScript((api) => {
   // hard dependencies, as in a minimum version of the "quasar"
   // package or a minimum version of Quasar App CLI
   api.compatibleWith('quasar', '^2.0.0')
-  api.compatibleWith('@quasar/app-vite', '^3.0.0-rc.1')
+  api.compatibleWith('@quasar/app-vite', '^3.0.0')
 
   // Here we extend /quasar.conf.js
   api.extendQuasarConf(extendQuasarConf)
--- my-starter-kit/typescript-variant/ae/src/install.ts
+++ my-starter-kit/typescript-variant/ae/src/install.ts
@@ -18,7 +18,7 @@ export default defineInstallScript((api) => {
   // hard dependencies, as in a minimum version of the "quasar"
   // package or a minimum version of Quasar App CLI
   api.compatibleWith("quasar", "^2.0.0");
-  api.compatibleWith("@quasar/app-vite", "^3.0.0-rc.1");
+  api.compatibleWith("@quasar/app-vite", "^3.0.0");
 
   // We render some files into the hosting project
 
